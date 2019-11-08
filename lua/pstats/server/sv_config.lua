3aa1959a06c3dd9e570282ebd2a3bf8c422827f3
TheNickSkater <beinik6@gmx.at>
2019-11-08 09:01:58 +0100

    Ups, whole config please

--- lua/pstats/server/sv_config.lua
+++ lua/pstats/server/sv_config.lua
@@ -12,7 +12,7 @@ PSTATS_DATA.config.SaveFrequency = "120"
 
 if not file.Exists("pstats/config.json", "DATA") then
   file.CreateDir("pstats")
-  file.Write("pstats/config.json", util.TableToJSON(PSTATS_DATA.config.MySQL, true))
+  file.Write("pstats/config.json", util.TableToJSON(PSTATS_DATA.config, true))
 else
   local configFile = file.Read("pstats/config.json", "DATA")
   if not configFile then return end
