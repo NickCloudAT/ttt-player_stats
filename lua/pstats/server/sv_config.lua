fef43e043b4b5156eb267ff7871af208fea6ab00
TheNickSkater <beinik6@gmx.at>
2019-11-08 09:00:46 +0100

    Added external config

--- lua/pstats/server/sv_config.lua
+++ lua/pstats/server/sv_config.lua
@@ -2,10 +2,20 @@ if not SERVER then return end
 
 PSTATS_DATA.config.MySQL = {
   HOST = "localhost",
-  PORT = 3306,
+  PORT = "3306",
   USERNAME = "dbuser",
   PASSWORD = "dbpassword",
   DATABASE = "db"
 }
 
-PSTATS_DATA.config.SaveFrequency = 120
+PSTATS_DATA.config.SaveFrequency = "120"
+
+if not file.Exists("pstats/config.json", "DATA") then
+  file.CreateDir("pstats")
+  file.Write("pstats/config.json", util.TableToJSON(PSTATS_DATA.config.MySQL, true))
+else
+  local configFile = file.Read("pstats/config.json", "DATA")
+  if not configFile then return end
+
+  PSTATS_DATA.config = util.JSONToTable(configFile)
+end
